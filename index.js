40d0acc793b93a58cad420543abb8ff832daa37f
sirazulsihab <smsisihab@gmail.com>
2021-11-01 22:45:07 +0600

    env added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient } = require('mongodb');
 
 const app = express();
@@ -10,7 +11,7 @@ app.use(express.json())
 // user : 
 // pass :FugxSnzA1NQWMmY4
 
-const uri = "mongodb+srv://sihab:FugxSnzA1NQWMmY4@cluster0.otdtg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.otdtg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
