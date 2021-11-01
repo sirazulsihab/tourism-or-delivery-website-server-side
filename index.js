f6eee62c76147d239256285b3aface3c2502efea
sirazulsihab <smsisihab@gmail.com>
2021-11-01 22:51:24 +0600

    env added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 // user : 
-// pass :FugxSnzA1NQWMmY4
+// pass :
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.otdtg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
